fbeb889a19f4986bbc2c00a19f9b4098ad8d1efb
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2020-09-13 16:50:31 +0800

    Foregin Key Issues in Model Builder

--- liteclerk-api/DBModelBuilder/TrnCounterReceiptModelBuilder.cs
+++ liteclerk-api/DBModelBuilder/TrnCounterReceiptModelBuilder.cs
@@ -17,10 +17,10 @@ namespace liteclerk_api.DBModelBuilder
                 entity.HasKey(e => e.Id);
                 
                 entity.Property(e => e.BranchId).HasColumnName("BranchId").HasColumnType("int").IsRequired();
-                entity.HasOne(f => f.MstCompanyBranch_BranchId).WithMany(f => f.TrnCounterReceipts_BranchId).HasForeignKey(f => f.BranchId).OnDelete(DeleteBehavior.Restrict);
+                //entity.HasOne(f => f.MstCompanyBranch_BranchId).WithMany(f => f.TrnCounterReceipts_BranchId).HasForeignKey(f => f.BranchId).OnDelete(DeleteBehavior.Restrict);
                 
                 entity.Property(e => e.CurrencyId).HasColumnName("CurrencyId").HasColumnType("int").IsRequired();
-                entity.HasOne(f => f.MstCurrency_CurrencyId).WithMany(f => f.TrnCounterReceipts_CurrencyId).HasForeignKey(f => f.CurrencyId).OnDelete(DeleteBehavior.Restrict);
+                //entity.HasOne(f => f.MstCurrency_CurrencyId).WithMany(f => f.TrnCounterReceipts_CurrencyId).HasForeignKey(f => f.CurrencyId).OnDelete(DeleteBehavior.Restrict);
                 
                 entity.Property(e => e.CRNumber).HasColumnName("CRNumber").HasColumnType("nvarchar(50)").HasMaxLength(50).IsRequired();
                 entity.Property(e => e.CRDate).HasColumnName("CRDate").HasColumnType("datetime").IsRequired();
@@ -37,11 +37,11 @@ namespace liteclerk_api.DBModelBuilder
                 entity.Property(e => e.IsLocked).HasColumnName("IsLocked").HasColumnType("bit").IsRequired();
 
                 entity.Property(e => e.CreatedByUserId).HasColumnName("CreatedByUserId").HasColumnType("int").IsRequired();
-                entity.HasOne(f => f.MstUser_CreatedByUserId).WithMany(f => f.TrnCounterReceipts_CreatedByUserId).HasForeignKey(f => f.CreatedByUserId).OnDelete(DeleteBehavior.Restrict);
+                //entity.HasOne(f => f.MstUser_CreatedByUserId).WithMany(f => f.TrnCounterReceipts_CreatedByUserId).HasForeignKey(f => f.CreatedByUserId).OnDelete(DeleteBehavior.Restrict);
                 entity.Property(e => e.CreatedDateTime).HasColumnName("CreatedDateTime").HasColumnType("datetime").IsRequired();
 
                 entity.Property(e => e.UpdatedByUserId).HasColumnName("UpdatedByUserId").HasColumnType("int").IsRequired();
-                entity.HasOne(f => f.MstUser_UpdatedByUserId).WithMany(f => f.TrnCounterReceipts_UpdatedByUserId).HasForeignKey(f => f.UpdatedByUserId).OnDelete(DeleteBehavior.Restrict);
+                //entity.HasOne(f => f.MstUser_UpdatedByUserId).WithMany(f => f.TrnCounterReceipts_UpdatedByUserId).HasForeignKey(f => f.UpdatedByUserId).OnDelete(DeleteBehavior.Restrict);
                 entity.Property(e => e.UpdatedDateTime).HasColumnName("UpdatedDateTime").HasColumnType("datetime").IsRequired();
             });
         }
