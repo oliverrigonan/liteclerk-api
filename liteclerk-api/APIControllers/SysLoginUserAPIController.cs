d8e5df77cef8373b5ab44578270857d4b57ca0cf
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2020-09-13 23:59:52 +0800

    Change Branch Done

---
+++ liteclerk-api/APIControllers/SysLoginUserAPIController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace liteclerk_api.APIControllers
+{
+    [Authorize]
+    [EnableCors("AppCorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SysLoginUserAPIController : ControllerBase
+    {
+        private readonly DBContext.LiteclerkDBContext _dbContext;
+
+        public SysLoginUserAPIController(DBContext.LiteclerkDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("detail")]
+        public async Task<ActionResult> GetLoginUserDetail()
+        {
+            try
+            {
+                Int32 loginUserId = Convert.ToInt32(User.FindFirst(ClaimTypes.Name)?.Value);
+
+                DBSets.MstUserDBSet loginUser = await (
+                    from d in _dbContext.MstUsers
+                    where d.Id == loginUserId
+                    select d
+                ).FirstOrDefaultAsync();
+
+                DTO.MstUserDTO user = await (
+                    from d in _dbContext.MstUsers
+                    where d.Id == loginUserId
+                    select new DTO.MstUserDTO
+                    {
+                        Id = d.Id,
+                        Username = d.Username,
+                        Fullname = d.Fullname,
+                        CompanyId = d.CompanyId,
+                        Company = new DTO.MstCompanyDTO
+                        {
+                            CompanyCode = d.MstCompany_CompanyId.CompanyCode,
+                            ManualCode = d.MstCompany_CompanyId.ManualCode,
+                            Company = d.MstCompany_CompanyId.Company
+                        },
+                        BranchId = d.BranchId,
+                        Branch = new DTO.MstCompanyBranchDTO
+                        {
+                            BranchCode = d.MstCompanyBranch_BranchId.BranchCode,
+                            ManualCode = d.MstCompanyBranch_BranchId.ManualCode,
+                            Branch = d.MstCompanyBranch_BranchId.Branch
+                        },
+                        IsActive = d.IsActive
+                    }
+                ).FirstOrDefaultAsync();
+
+                return StatusCode(200, user);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.InnerException.Message);
+            }
+        }
+
+        [HttpPut("changeBranch/{branchId}")]
+        public async Task<ActionResult> ChangeBranch(Int32 branchId)
+        {
+            try
+            {
+                Int32 loginUserId = Convert.ToInt32(User.FindFirst(ClaimTypes.Name)?.Value);
+
+                DBSets.MstUserDBSet loginUser = await (
+                    from d in _dbContext.MstUsers
+                    where d.Id == loginUserId
+                    select d
+                ).FirstOrDefaultAsync();
+
+                if (loginUser == null)
+                {
+                    return StatusCode(404, "Login user not found.");
+                }
+
+                DBSets.MstCompanyBranchDBSet branch = await (
+                    from d in _dbContext.MstCompanyBranches
+                    where d.Id == branchId
+                    && d.MstCompany_CompanyId.IsLocked == true
+                    select d
+                ).FirstOrDefaultAsync();
+
+                if (branch == null)
+                {
+                    return StatusCode(404, "Branch not found.");
+                }
+
+                DBSets.MstUserDBSet changeBranch = loginUser;
+                changeBranch.BranchId = branchId;
+
+                await _dbContext.SaveChangesAsync();
+
+                return StatusCode(200);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.InnerException.Message);
+            }
+        }
+    }
+}
--- liteclerk-api/DTO/SysUserAuthenticationResponseDTO.cs
+++ liteclerk-api/DTO/SysUserAuthenticationResponseDTO.cs
@@ -12,6 +12,8 @@ namespace liteclerk_api.DTO
         public String ExpiresIn { get; set; }
         public String UserName { get; set; }
         public String FullName { get; set; }
+        public String Company { get; set; }
+        public String Branch { get; set; }
 
         public SysUserAuthenticationResponseDTO(DBSets.MstUserDBSet mstUserDBSet, String accessToken, String expiresIn)
         {
@@ -20,6 +22,8 @@ namespace liteclerk_api.DTO
             ExpiresIn = expiresIn;
             UserName = mstUserDBSet.Username;
             FullName = mstUserDBSet.Fullname;
+            Company = mstUserDBSet.CompanyId != null ? mstUserDBSet.MstCompany_CompanyId.Company : "";
+            Branch = mstUserDBSet.BranchId != null ? mstUserDBSet.MstCompanyBranch_BranchId.Branch : "";
         }
     }
 }
