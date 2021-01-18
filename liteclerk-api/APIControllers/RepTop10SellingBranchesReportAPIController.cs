4c023612b5fdf8171f0bed76e4c156c6b89da474
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2021-01-18 23:55:05 +0800

    Top 10 selling reports for dashboard

---
+++ liteclerk-api/APIControllers/RepTop10SellingBranchesReportAPIController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace liteclerk_api.APIControllers
+{
+    [Authorize]
+    [EnableCors("AppCorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepTop10SellingBranchesReportAPIController : ControllerBase
+    {
+        private readonly DBContext.LiteclerkDBContext _dbContext;
+
+        public RepTop10SellingBranchesReportAPIController(DBContext.LiteclerkDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("list/byDateRange/{startDate}/{endDate}")]
+        public async Task<ActionResult> GetTop10SellingBranchesReportList(String startDate, String endDate)
+        {
+            try
+            {
+                Task<List<DTO.TrnSalesInvoiceDTO>> taskTop10SellingBranchesReportList = Task.FromResult(new List<DTO.TrnSalesInvoiceDTO>());
+
+                Int32 loginUserId = Convert.ToInt32(User.FindFirst(ClaimTypes.Name)?.Value);
+
+                var loginUser = await (
+                    from d in _dbContext.MstUsers
+                    where d.Id == loginUserId
+                    select d
+                ).FirstOrDefaultAsync();
+
+                var salesInvoices = await (
+                    from d in _dbContext.TrnSalesInvoices
+                    where d.SIDate >= Convert.ToDateTime(startDate)
+                    && d.SIDate <= Convert.ToDateTime(endDate)
+                    && d.MstCompanyBranch_BranchId.CompanyId == loginUser.CompanyId
+                    && d.IsLocked == true
+                    select d
+                ).ToListAsync();
+
+                if (salesInvoices.Any())
+                {
+                    var groupSalesInvoices = from d in salesInvoices
+                                             group d by new
+                                             {
+                                                 Branch = d.MstCompanyBranch_BranchId.Branch
+                                             } into g
+                                             select new DTO.TrnSalesInvoiceDTO
+                                             {
+                                                 Branch = new DTO.MstCompanyBranchDTO
+                                                 {
+                                                     Branch = g.Key.Branch
+                                                 },
+                                                 Amount = g.Sum(d => d.Amount)
+                                             };
+
+                    taskTop10SellingBranchesReportList = Task.FromResult(groupSalesInvoices.OrderByDescending(d => d.Amount).Take(10).ToList());
+                }
+
+                return StatusCode(200, taskTop10SellingBranchesReportList.Result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.InnerException.Message);
+            }
+        }
+    }
+}
---
+++ liteclerk-api/APIControllers/RepTop10SellingItemsReportAPIController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace liteclerk_api.APIControllers
+{
+    [Authorize]
+    [EnableCors("AppCorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepTop10SellingItemsReportAPIController : ControllerBase
+    {
+        private readonly DBContext.LiteclerkDBContext _dbContext;
+
+        public RepTop10SellingItemsReportAPIController(DBContext.LiteclerkDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("list/byDateRange/{startDate}/{endDate}")]
+        public async Task<ActionResult> GetTop10SellingItemsReportList(String startDate, String endDate)
+        {
+            try
+            {
+                Task<List<DTO.TrnSalesInvoiceItemDTO>> taskTop10SellingItemsReportList = Task.FromResult(new List<DTO.TrnSalesInvoiceItemDTO>());
+
+                Int32 loginUserId = Convert.ToInt32(User.FindFirst(ClaimTypes.Name)?.Value);
+
+                var loginUser = await (
+                    from d in _dbContext.MstUsers
+                    where d.Id == loginUserId
+                    select d
+                ).FirstOrDefaultAsync();
+
+                var salesInvoiceItems = await (
+                    from d in _dbContext.TrnSalesInvoiceItems
+                    where d.TrnSalesInvoice_SIId.SIDate >= Convert.ToDateTime(startDate)
+                    && d.TrnSalesInvoice_SIId.SIDate <= Convert.ToDateTime(endDate)
+                    && d.TrnSalesInvoice_SIId.MstCompanyBranch_BranchId.CompanyId == loginUser.CompanyId
+                    && d.TrnSalesInvoice_SIId.BranchId == loginUser.BranchId
+                    && d.TrnSalesInvoice_SIId.IsLocked == true
+                    && d.MstArticle_ItemId.MstArticleItems_ArticleId.Any() == true
+                    select d
+                ).ToListAsync();
+
+                if (salesInvoiceItems.Any())
+                {
+                    var groupSalesInvoiceItems = from d in salesInvoiceItems
+                                                 group d by new
+                                                 {
+                                                     ItemDescription = d.MstArticle_ItemId.MstArticleItems_ArticleId.FirstOrDefault().Description
+                                                 } into g
+                                                 select new DTO.TrnSalesInvoiceItemDTO
+                                                 {
+                                                     Item = new DTO.MstArticleItemDTO
+                                                     {
+                                                         Description = g.Key.ItemDescription
+                                                     },
+                                                     Quantity = g.Sum(d => d.Quantity)
+                                                 };
+
+                    taskTop10SellingItemsReportList = Task.FromResult(groupSalesInvoiceItems.OrderByDescending(d => d.Quantity).Take(10).ToList());
+                }
+
+                return StatusCode(200, taskTop10SellingItemsReportList.Result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.InnerException.Message);
+            }
+        }
+    }
+}
